0d066774acb721effafccf4dfd0083a4d975f324
noah-mccaskill <noah-mccaskill@users.noreply.github.com>
2022-10-03 22:18:22 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -17,37 +17,36 @@ const prompt = promptSync()
  * @returns {mumber} userLength of the wood
  */
 function BoardFoot(userHeight: number, userWidth: number): number {
-//Process
-const BOARD_FOOT = 144
-const userLength = BOARD_FOOT / (userWidth * userHeight)
-return userLength
+  //Process
+  const BOARD_FOOT = 144
+  const userLength = BOARD_FOOT / (userWidth * userHeight)
+  return userLength
 }
 
 console.log('This program calculates the required length for a board foot.')
 
 try {
-//Input and Output
-const valueInput = prompt("Enter in a width: ");
-const userWidth = parseFloat(valueInput);
-const valueInputTwo = prompt("Enter a height: ");
-const userHeight = parseFloat(valueInputTwo);
+  //Input and Output
+  const valueInput = prompt('Enter in a width: ')
+  const userWidth = parseFloat(valueInput)
+  const valueInputTwo = prompt('Enter a height: ')
+  const userHeight = parseFloat(valueInputTwo)
 
-if (isNaN(userWidth) || isNaN(userHeight)) {
-	throw new Error("\nThat is not a valid input.");
-}
+  if (isNaN(userWidth) || isNaN(userHeight)) {
+    throw new Error('\nThat is not a valid input.')
+  }
 
-if (userWidth <= 0 || userHeight <= 0) {
-	throw new Error("\nThat is not a valid input.");
-}
+  if (userWidth <= 0 || userHeight <= 0) {
+    throw new Error('\nThat is not a valid input.')
+  }
 
-console.log(
-	`\nThe required length  is ${BoardFoot(
-		userHeight,
-		userWidth
-	)} inches for the board foot to be 144 inches³.`
-);
-console.log("\nDone.");
+  console.log(
+    `\nThe required length  is ${BoardFoot(
+      userHeight,
+      userWidth
+    )} inches for the board foot to be 144 inches³.`
+  )
+  console.log('\nDone.')
 } catch (e) {
-	console.log("\nThat is not a valid input.");
+  console.log('\nThat is not a valid input.')
 }
-
